fd2f252649644852904b27d6cbdcf92fdc1ad128
Eduard Chelebian <70700917+eduardchelebian@users.noreply.github.com>
2022-08-09 10:10:31 +0200

    update utils

--- labs/iss_utils/starfish2tmap.py
+++ labs/iss_utils/starfish2tmap.py
@@ -1,3 +1,4 @@
+import os
 import numpy as np
 import pandas as pd
 
@@ -5,7 +6,7 @@ from PIL import Image
 
 from starfish.types import Axes
 
-def qc_csv(experiment, spot_intensities, output_name):
+def qc_csv(experiment, spot_intensities, output_path):
 
     """
     Create a CSV file from a starfish experiments compatible with the TissUUmaps "Spot Insepector" plugin
@@ -13,9 +14,11 @@ def qc_csv(experiment, spot_intensities, output_name):
     Parameters:
         experiment (starfish Experiment): containing the codebook
         spot_intensities (starfish DecodedIntensityTable): result of the decoding process
-        output_name (string): name of the output csv file
+        output_path (string): directory where you wish to save the images
     """
- 
+    
+    output_path = os.path.abspath(output_path)
+    csv_path = os.path.join(output_path, 'pixel_decoding.csv')
     rounds_gt = []
     channels_gt = []
 
@@ -35,37 +38,39 @@ def qc_csv(experiment, spot_intensities, output_name):
         channels_gt.append('{};{};{};{}'.format(chs[0][0],chs[1][0],chs[2][0],chs[3][0]))
 
     df = pd.DataFrame(np.stack([x,y,target_names,rounds_gt,channels_gt]).transpose(), columns=['x','y','target_name','rounds','channels'])
-    df.to_csv(output_name)
+    df.to_csv(csv_path)
 
-    return output_name
+    return csv_path
 
 
 
-def qc_images(filtered_imgs):
+def qc_images(filtered_imgs, exp, output_path):
 
     """
     Creates the images from a starfish experiments compatible with the TissUUmaps "Spot Insepector" plugin
 
     Parameters:
         filtered_imgs (starfish ImageStack): image stack after filtering and deconvolving the data
+        output_path (string): directory where you wish to save the images
     """
 
     image_names = []
+    dapis = exp['fov_001'].get_image('nuclei')
+    output_path = os.path.abspath(output_path)
 
     for r in range(filtered_imgs.num_rounds):
         for c in range(filtered_imgs.num_chs):
             im = np.squeeze(filtered_imgs.sel({Axes.CH: c, Axes.ROUND: r}).xarray.values)
-            # im = np.log(2**16*im+1)
-
-            # mn = im.min()
-            # mx = im.max()
-            # mx -= mn
-            # im = ((im - mn)/mx) * 255
-            # im = im.astype(np.uint8)
-
             im = Image.fromarray(im)
             image_name = 'Round{}_{}.tif'.format(r,c)
-            im.save(image_name)
-            image_names.append(image_name)
+            image_path = os.path.join(output_path, image_name)
+            im.save(image_path)
+            image_names.append(image_path)
+        dapi = np.squeeze(dapis.sel({Axes.ROUND: r}).xarray.values)
+        dapi = Image.fromarray(dapi)
+        dapi_name = 'Round{}_{}.tif'.format(r,4)
+        dapi_path = os.path.join(output_path, dapi_name)
+        dapi.save(dapi_path)
+        image_names.append(dapi_path)
 
     return image_names
